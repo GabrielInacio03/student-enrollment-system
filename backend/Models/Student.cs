ac07e2043798464fa5e516fe28f89251e37da56e
GabrielInacio03 <inaciogs03@gmail.com>
2025-08-09 20:38:53 -0300

    feat: modificando a estrutura do backend com confiig do gitignore

---
+++ backend/Models/Student.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string RA { get; set; }
+        public string CPF { get; set; }
+
+
+    }
+}
